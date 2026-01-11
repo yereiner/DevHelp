7838f896bbcfc356e40fecb24ce74905f73e4f69
Marco <marcoszxdev@gmail.com>
2026-01-10 21:40:17 -0500

    add src files se modifico la estructura de el proyecto por una un poco mas profecional

---
+++ src/search.py
@@ -0,0 +1,21 @@
+from core.loader import cargar_json 
+import os 
+
+def buscar(lenguaje, termino):
+
+    lenguaje = lenguaje.lower()
+    termino = termino.lower()
+
+    ruta_relativa = os.path.join(
+        "data",
+        lenguaje,
+        f"{termino}.json"
+    )
+
+    try:
+        datos = cargar_json(ruta_relativa)
+        return datos
+    except FileNotFoundError:
+        return None
+
+
